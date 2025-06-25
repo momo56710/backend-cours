d69d029c29e65f5b7b5113096990947f33cbbc19
momo56710 <mohamedmouh567@gmail.com>
2025-06-25 17:34:18 +0100

    fix: update comment in usersRoutes.js for clarity

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,5 +4,5 @@ const userController = require("../controllers/userController");
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
 router.post("/", userController.createUser);
-//hello this is a comment
+//hello this is another comment 
 module.exports = router;
