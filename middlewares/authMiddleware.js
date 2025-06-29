298b615c5339892dae8db1738587dafb438497d4
momo56710 <mohamedmouh567@gmail.com>
2025-06-29 16:30:57 +0100

    fix: change response status code to 401 for missing access token in auth middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(200).json({
+    return res.status(401).json({
       success: false,
       message: "Access token required",
     });
